91a3b4625ffb27c5471741fb858de7f6a3d957b7
katafuchix <katafuchix@gmail.com>
2022-01-01 02:55:27 +0900

    Initial Commit

---
+++ SwiftUI_Sample_Login/SwiftUI_Sample_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sample_LoginApp.swift
+//  SwiftUI_Sample_Login
+//
+//  Created by cano on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
